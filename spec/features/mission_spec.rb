a81dc3c2a6e59d6fa6199eaaef7defd607f0dc6b
Skye <skyee20211@gmail.com>
2021-07-15 22:33:55 +0800

    little test

--- spec/features/mission_spec.rb
+++ spec/features/mission_spec.rb
@@ -1,30 +1,24 @@
 require 'rails_helper'
-require 'selenium/webdriver'
+# require 'selenium/webdriver'
 
 
 feature "mission", :type => :feature do
-  
+
 
   scenario "ordered by created_at" do
+    m1 = Mission.create!(title: "m1")
+    m2 = Mission.create!(title: "m2")
 
     visit "/" #首頁
     expect(page).to have_content("新增任務")
+    expect(page.body).to match(/m1.*m2/m)
     
     visit "/missions/new"
     expect(page).to have_content("任務名稱")
     expect(page).to have_content("返回任務列表")
-  end
 
-end
-
-describe "mission list" do
-  
-  
-  scenario "order by created_at", js: true do
-    m1 = Mission.create!(title: "m1")
-    m2 = Mission.create!(title: "m2")
     
-    visit "/"
-    expect(page.body).to match(/m1.*m2/m)
+
   end
+
 end
